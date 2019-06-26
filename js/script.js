6bf306385acb3dceca7ebe98c8bb984729e065bd
elwira-m <36117564+elwira-m@users.noreply.github.com>
2019-06-27 01:56:53 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+///////////////////////////PORTFOLIO//////////////////////
+
+$(function() {
+    $('.icon').click(function() {
+        $('#myTopnav').toggleClass('nav-responsive');
+    }); 
+    
+});
